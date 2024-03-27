89ec4cc79124a1769ec665e73e89dfe6605ebcf2
Torsten Sommer <torsten.sommer@3ds.com>
2024-03-27 15:20:00 +0100

    Add templates to distribution

--- setup.py
+++ setup.py
@@ -3,5 +3,6 @@ from setuptools import setup
 setup(
     name='modelica_fmi',
     packages=['modelica_fmi'],
+    package_data={'modelica_fmi': ['templates/*.mo']},
     install_requires=['fmpy']
 )
