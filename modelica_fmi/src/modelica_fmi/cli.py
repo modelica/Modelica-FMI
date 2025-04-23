27862eb92ea2ccf6337465c0f9c7e884cc708485
Torsten Sommer <torsten.sommer@3ds.com>
2025-04-23 15:39:33 +0200

    Add -v switch

--- modelica_fmi/src/modelica_fmi/cli.py
+++ modelica_fmi/src/modelica_fmi/cli.py
@@ -3,12 +3,16 @@
 def main():
 
     import argparse
+    from . import __version__ as version
     from .import_fmu_to_modelica import import_fmu_to_modelica
 
     parser = argparse.ArgumentParser(
+        prog='modelica-fmi',
         description="Import an FMU into a Modelica library",
         formatter_class=argparse.RawDescriptionHelpFormatter
     )
+
+    parser.add_argument('-v', '--version', action='version', version=f'%(prog)s version {version}', help="print the program version")
 
     parser.add_argument('fmu_path', help="Path of the FMU to import")
     parser.add_argument('model_path', help="Path of the Modelica file to generate")
