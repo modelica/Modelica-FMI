eae04d22ecf52f11d6667cbdaf55f1d8326ccd02
Torsten Sommer <torsten.sommer@3ds.com>
2026-09-15 11:53:45 +0200

    Add dummy library for tests

--- rust/modelica-fmi/tests/path_tests.rs
+++ rust/modelica-fmi/tests/path_tests.rs
@@ -1,21 +1,25 @@
-use std::path::Path;
+use std::path::{Path, PathBuf};
+
+fn test_library_root() -> PathBuf {
+    Path::new(env!("CARGO_MANIFEST_DIR")).join("tests/resources")
+}
 
 #[test]
 fn computes_modelica_within_path_for_nested_library_packages() {
-    let model_path = Path::new(r"E:\WS\Modelica-FMI\FMI\Examples\FMI2\Controller_FMU_2.mo");
+    let model_path = test_library_root().join("MyLibrary/MyPackage/Controller_FMU_2.mo");
 
     assert_eq!(
-        modelica_fmi::modelica_within_path(model_path).unwrap(),
-        "FMI.Examples.FMI2".to_owned()
+        modelica_fmi::modelica_within_path(model_path.as_path()).unwrap(),
+        "MyLibrary.MyPackage".to_owned()
     );
 }
 
 #[test]
 fn computes_modelica_within_path_standalone_model() {
-    let model_path = Path::new(r"C:\Users\tsr2\Documents\Dymola\Controller_FMU_2.mo");
+    let model_path = test_library_root().join("Controller_FMU_2.mo");
 
     assert_eq!(
-        modelica_fmi::modelica_within_path(model_path).unwrap(),
+        modelica_fmi::modelica_within_path(model_path.as_path()).unwrap(),
         String::new()
     );
 }
